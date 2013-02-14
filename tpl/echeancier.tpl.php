11c85c95b7bd94253aa55a3ce9a085ac183db29f
Maxime Kohlhaas <mko@atm-consulting.fr>
2013-02-14 15:33:42 +0100

    Mise en forme

--- tpl/echeancier.tpl.php
+++ tpl/echeancier.tpl.php
@@ -11,9 +11,9 @@
 		<td align="right">Loyers<br />TTC</td>
 	</tr>
 
-	<tr class="ligne">
+	<tr class="pair">
 		<td colspan="2">&nbsp;</td>
-		<td align="right"><strong>[autre.capitalInit; frm='0,00 €']</strong></td>
+		<td align="right"><strong>[autre.capitalInit; frm='0 000,00 €']</strong></td>
 		<td colspan="5">&nbsp;</td>
 	</tr>
 
@@ -21,27 +21,26 @@
 	<tr class="impair">
 		<td>[ligne.#; block=tr]</td>
 		<td>[ligne.date]</td>
-		<td align="right"><strong>[ligne.capital; frm='0,00 €']</strong></td>
-		<td align="right">[ligne.amortissement; frm='0,00 €']</td>
-		<td align="right">[ligne.interet; frm='0,00 €']</td>
-		<td align="right">[ligne.assurance; frm='0,00 €']</td>
-		<td align="right">[ligne.loyerHT; frm='0,00 €']</td>
-		<td align="right">[ligne.loyer; frm='0,00 €']</td>
+		<td align="right"><strong>[ligne.capital; frm='0 000,00 €']</strong></td>
+		<td align="right">[ligne.amortissement; frm='0 000,00 €']</td>
+		<td align="right">[ligne.interet; frm='0 000,00 €']</td>
+		<td align="right">[ligne.assurance; frm='0 000,00 €']</td>
+		<td align="right">[ligne.loyerHT; frm='0 000,00 €']</td>
+		<td align="right">[ligne.loyer; frm='0 000,00 €']</td>
 	</tr>
 	<tr class="pair">
 		<td>[ligne.#; block=tr]</td>
 		<td>[ligne.date]</td>
-		<td align="right"><strong>[ligne.capital; frm='0,00 €']</strong></td>
-		<td align="right">[ligne.amortissement; frm='0,00 €']</td>
-		<td align="right">[ligne.interet; frm='0,00 €']</td>
-		<td align="right">[ligne.assurance; frm='0,00 €']</td>
-		<td align="right">[ligne.loyerHT; frm='0,00 €']</td>
-		<td align="right">[ligne.loyer; frm='0,00 €']</td>
+		<td align="right"><strong>[ligne.capital; frm='0 000,00 €']</strong></td>
+		<td align="right">[ligne.amortissement; frm='0 000,00 €']</td>
+		<td align="right">[ligne.interet; frm='0 000,00 €']</td>
+		<td align="right">[ligne.assurance; frm='0 000,00 €']</td>
+		<td align="right">[ligne.loyerHT; frm='0 000,00 €']</td>
+		<td align="right">[ligne.loyer; frm='0 000,00 €']</td>
 	</tr>
 	
 	<tr class="ligne">
-		<td colspan="5">&nbsp;</td>
-		<td>Val. Résiduelle</td>
+		<td align="right" colspan="6">Val. Résiduelle</td>
 		<td align="right">[autre.reste; frm='0,00 €']</td>
 		<td align="right">[autre.resteTTC; frm='0,00 €']</td>
 	</tr>
