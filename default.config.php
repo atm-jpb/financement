1993f069f03c08c70a8b1f29024570ff6b62ee2d
Maxime Kohlhaas <mko@atm-consulting.fr>
2013-05-31 09:47:56 +0200

    Change config for inc

--- default.config.php
+++ default.config.php
@@ -8,9 +8,9 @@
 	if(defined('INC_FROM_CRON_SCRIPT')) {
 		include(ROOT."master.inc.php");
 	}
-	else {
+	else if(!defined('INC_FROM_DOLIBARR')) {
 		include(ROOT."main.inc.php");
-
+		
 		define('DB_HOST',$dolibarr_main_db_host);
 		define('DB_NAME',$dolibarr_main_db_name);
 		define('DB_USER',$dolibarr_main_db_user);
