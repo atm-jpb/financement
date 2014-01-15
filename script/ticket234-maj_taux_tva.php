f95fa6df4997f11325e61350cffcf13f261cd370
root <root@SRVDOLIBARR.groupecpro.local>
2014-01-15 10:54:29 +0100

    debug #234

--- script/ticket234-maj_taux_tva.php
+++ script/ticket234-maj_taux_tva.php
@@ -1,9 +1,9 @@
 <?php
-	require("../../config.php");
+	require("../config.php");
 	//require('config.php');
 	
 	include_once(DOL_DOCUMENT_ROOT."/fourn/class/fournisseur.facture.class.php");
-
+	include_once(DOL_DOCUMENT_ROOT."/product/class/product.class.php");
 	global $db;
 	
 	$ATMdb = new Tdb;
@@ -30,16 +30,20 @@
 		$facture = new FactureFournisseur($db);
 		$facture->fetch($ATMdb->Get_field('idCmd'));
 
+		print "Facture ".$facture->id.'<br />';
+
 		$TLigne =$facture->fetch_lines();
 		
-		foreach($TLigne as $line) {
+//		var_dump($TLigne);
+
+		foreach($facture->lines as $line) {
 			print "Mise à jour de ligne de facture(".$line->rowid.") pour facture ".$facture->id." (".$facture->ref.")<br />";
-			//$facture->updateline($line->rowid, $line->libelle, $line->pu_ht, 20.0, $line->localtax1_tx, $line->localtax2_tx, $line->qty, $line->fk_product);			
+			$facture->updateline($line->rowid, $line->libelle, $line->pu_ht, 20.0, $line->localtax1_tx, $line->localtax2_tx, $line->qty, $line->fk_product);			
 			
 		}
 
-		//$facture->set_unpaid($user);
-
+		$facture->set_unpaid($user);
+	//	exit;
 	}
 	
 	
