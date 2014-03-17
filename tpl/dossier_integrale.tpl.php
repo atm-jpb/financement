b9b1149af34de604dc30b4487ec04d80c0ef22b6
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-03-17 16:35:05 +0100

    Cosmétique intégrale

--- tpl/dossier_integrale.tpl.php
+++ tpl/dossier_integrale.tpl.php
@@ -1,75 +1,81 @@
-<table class="border" width="100%">
-	<tr class="liste_titre">
-		<td colspan="4">Informations dossier</td>
-	</tr>
-	<tr>
-		<td width="20%">Nom du tiers</td>
-		<td width="30%">[client.societe; strconv=no] [client.autres_simul; strconv=no]</a></td>
-		<td width="20%">Contact</td>
-		<td>[client.contact_externe]</td>
-	</tr>
-	<tr>
-		<td>Adresse</td>
-		<td>[client.adresse]</td>
-		<td>Code Artis</td>
-		<td>[client.code_client]</td>
-	</tr>
-	<tr>
-		<td>CP / Ville</td>
-		<td>[client.cpville]</td>
-		<td>SIRET</td>
-		<td>[client.siret]</td>
-	</tr>
-	<tr>
-		<td>&nbsp;</td>
-		<td>&nbsp;</td>
-		<td>Code NAF</td>
-		<td>[client.naf]</td>
-	</tr>
-	<tr class="liste_titre">
-		<td colspan="4">Suivi intégrale</td>
-	</tr>
-	<tr>
-		<td colspan="4">
-			<table class="border" width="100%">
-			<tr class="liste_titre">
-				<td>Période</td>
-				<td>Vol noir engag&eacute;</td>
-				<td>Vol noir r&eacute;alis&eacute;</td>
-				<td>Vol noir factur&eacute;</td>
-				<td>Coût unitaire noir</td>
-				<td>Vol couleur engag&eacute;</td>
-				<td>Vol couleur r&eacute;alis&eacute;</td>
-				<td>Vol couleur factur&eacute;</td>
-				<td>Coût unitaire couleur</td>
-				<td>FAS</td>
-				<td>FASS</td>
-				<td>Frais de dossier</td>
-				<td>Bris de machine</td>
-				<td>FTC</td>
-				<td>Total HT engag&eacute;</td>
-				<td>Total HT r&eacute;alis&eacute;</td>
-				<td>%</td>
-			</tr>
-			<tr>
-				<td>[integrale.periode;block=tr]</td>
-				<td>[integrale.vol_noir_engage]</td>
-				<td>[integrale.vol_noir_realise]</td>
-				<td>[integrale.vol_noir_facture]</td>
-				<td>[integrale.cout_unit_noir]</td>
-				<td>[integrale.vol_coul_engage]</td>
-				<td>[integrale.vol_coul_realise]</td>
-				<td>[integrale.vol_coul_facture]</td>
-				<td>[integrale.cout_unit_coul]</td>
-				<td>[integrale.fas]</td>
-				<td>[integrale.fass]</td>
-				<td>[integrale.frais_dossier]</td>
-				<td>[integrale.frais_bris_machine]</td>
-				<td>[integrale.frais_facturation]</td>
-				<td>[integrale.total_ht_engage]</td>
-				<td>[integrale.total_ht_realise]</td>
-				<td>[integrale.ecart]</td>
-			</tr>
-		</td>
-	</tr>
-</table>
+<div class="tabs">
+	<a class="tabTitle"><img border="0" title="" alt="" src="./img/object_financeico.png"> Dossier</a>
+	<a href="dossier.php?id=[dossier.rowid]" class="tab">Fiche</a>
+	<a href="dossier_integrale.php?id=[dossier.rowid]" class="tab" id="active">Suivi int&eacute;grale</a>
+	<a href="[dossier.url_therefore][fin.reference]" class="tab" target="_blank">Therefore</a>
+</div>
+	
+<div class="tabBar">
+
+	<table class="border" width="100%">
+		<tr class="liste_titre">
+			<td colspan="4">Informations dossier</td>
+		</tr>
+		<tr>
+			<td width="20%">Nom du tiers</td>
+			<td width="30%">[client.nom; strconv=no]</a></td>
+		</tr>
+		<tr>
+			<td>N&deg; contrat C'Pro</td>
+			<td>[fin.reference]</td>
+		</tr>
+		<tr>
+			<td>Date de début</td>
+			<td>[fin.date_debut; frm=dd/mm/yy]</td>
+		</tr>
+		<tr>
+			<td>Terme</td>
+			<td>[fin._affterme]</td>
+		</tr>
+		<tr>
+			<td>Facturation</td>
+			<td>[fin._affperiodicite]</td>
+		</tr>
+		<tr>
+			<td>Durée</td>
+			<td>[fin.duree]</td>
+		</tr>
+	</table>
+	<br>
+	<table class="border" width="100%">
+		<tr class="liste_titre">
+			<td align="center">Période</td>
+			<td align="center">Vol noir engag&eacute;</td>
+			<td align="center">Vol noir r&eacute;alis&eacute;</td>
+			<td align="center">Vol noir factur&eacute;</td>
+			<td align="center">Coût unitaire noir</td>
+			<td align="center">Vol couleur engag&eacute;</td>
+			<td align="center">Vol couleur r&eacute;alis&eacute;</td>
+			<td align="center">Vol couleur factur&eacute;</td>
+			<td align="center">Coût unitaire couleur</td>
+			<td align="center">FAS</td>
+			<td align="center">FASS</td>
+			<td align="center">Frais de dossier</td>
+			<td align="center">Bris de machine</td>
+			<td align="center">FTC</td>
+			<td align="center">Total HT engag&eacute;</td>
+			<td align="center">Total HT r&eacute;alis&eacute;</td>
+			<td align="center">%</td>
+		</tr>
+		<tr>
+			<td align="center">[integrale.periode;block=tr]</td>
+			<td align="center">[integrale.vol_noir_engage]</td>
+			<td align="center">[integrale.vol_noir_realise]</td>
+			<td align="center">[integrale.vol_noir_facture]</td>
+			<td align="right">[integrale.cout_unit_noir;frm=0 000,00000] &euro;</td>
+			<td align="center">[integrale.vol_coul_engage]</td>
+			<td align="center">[integrale.vol_coul_realise]</td>
+			<td align="center">[integrale.vol_coul_facture]</td>
+			<td align="right">[integrale.cout_unit_coul;frm=0 000,00000] &euro;</td>
+			<td align="right">[integrale.fas;frm=0 000,00] &euro;</td>
+			<td align="right">[integrale.fass;frm=0 000,00] &euro;</td>
+			<td align="right">[integrale.frais_dossier;frm=0 000,00] &euro;</td>
+			<td align="right">[integrale.frais_bris_machine;frm=0 000,00] &euro;</td>
+			<td align="right">[integrale.frais_facturation;frm=0 000,00] &euro;</td>
+			<td align="right">[integrale.total_ht_engage;frm=0 000,00] &euro;</td>
+			<td align="right">[integrale.total_ht_realise;frm=0 000,00] &euro;</td>
+			<td align="center">[integrale.ecart;frm=0 000,00] %</td>
+		</tr>
+	</table>
+
+</div>
