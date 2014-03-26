72c068e9e5cc1772328db660396382dc909acff1
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-03-26 17:25:22 +0100

    debug

--- simulation.php
+++ simulation.php
@@ -374,7 +374,7 @@ function _fiche(&$ATMdb, &$simulation, $mode) {
 	if($user->rights->financement->admin->write && ($mode == "add" || $mode == "new" || $mode == "edit")){
 		$formdolibarr = new Form($db);
 		$rachat_autres = "texte";
-		$TUserExculde = array();
+		$TUserInclude = array();
 		$TUserInclude = TRequeteCore::_get_id_by_sql($ATMdb, "SELECT u.rowid 
 															FROM ".MAIN_DB_PREFIX."user as u 
 																LEFT JOIN ".MAIN_DB_PREFIX."usergroup_user as ugu ON (ugu.fk_user = u.rowid)
@@ -383,7 +383,7 @@ function _fiche(&$ATMdb, &$simulation, $mode) {
 																  ug.nom = 'GSL_DOLIBARR_FINANCEMENT_ADV' OR
 																  ug.nom = 'GSL_DOLIBARR_FINANCEMENT_COMMERCIAL'");
 		//pre($TUserExculde,true); exit;
-		$link_user = $formdolibarr->select_dolusers($simulation->fk_user_author,'fk_user_author',1,$TUserInclude,0,'','',$conf->entity);
+		$link_user = $formdolibarr->select_dolusers($simulation->fk_user_author,'fk_user_author',1,'',0,$TUserInclude,'',$conf->entity);
 	}
 	else{
 		$rachat_autres = "texteRO";
