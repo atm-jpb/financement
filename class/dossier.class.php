1ae256b09af83cabf32ee1d715da914af801f194
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-12-08 10:58:15 +0100

    Amélioration affichage

--- class/dossier.class.php
+++ class/dossier.class.php
@@ -636,7 +636,7 @@ class TFin_dossier extends TObjetStd {
 				$data['facture_link'] = ($type_echeancier == 'CLIENT') ? DOL_URL_ROOT.'/compta/facture.php?facid=' : DOL_URL_ROOT.'/fourn/facture/fiche.php?facid=';
 				$data['facture_link'] .= $fact->id;
 				$data['facture_bg'] = ($fact->paye == 1) ? '#00FF00' : '#FF0000';
-			} else if($type_echeancier == 'LEASER' && $this->nature_financement == 'INTERNE' && $time < time()) {
+			} else if($type_echeancier == 'LEASER' && $this->nature_financement == 'INTERNE' && $time < time() && $this->date_solde == 0 && $this->montant_solde == 0) {
 				$link = dol_buildpath('/financement/dossier.php?action=new_facture_leaser&id_dossier='.$this->rowid.'&echeance='.($i+1),1);
 				$data['facture_total_ht'] = '+';
 				$data['facture_link'] = $link;
