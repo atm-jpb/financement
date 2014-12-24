5b3fa4ae6780c6b80725bd135be81e6128858a1f
root <root@SRVDOLIBARR.groupecpro.local>
2014-12-24 11:11:40 +0100

    suppression htmlentities tout pourri

--- class/affaire.class.php
+++ class/affaire.class.php
@@ -454,7 +454,8 @@ class TFin_affaire extends TObjetStd {
 			);
 		
 		$bien->appendChild($xml->createElement("immobilisation",$a+1));
-		$bien->appendChild($xml->createElement("designation1",substr(htmlentities($product->label),0,30)));
+		//$bien->appendChild($xml->createElement("designation1",substr(htmlentities($product->label),0,30)));
+		$bien->appendChild($xml->createElement("designation1",substr($product->label,0,30)));
 		$bien->appendChild($xml->createElement("noSerie",$assetLink->asset->serial_number));
 		$bien->appendChild($xml->createElement("immatriculable","NON"));
 		$bien->appendChild($xml->createElement("codeAssietteTheorique","U03C"));
