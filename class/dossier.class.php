3c646d3db0bf58d4ff372dda48915497f5909862
atm <geoffrey@atm-consulting.fr>
2015-02-27 10:20:08 +0100

    Debug

--- class/dossier.class.php
+++ class/dossier.class.php
@@ -916,8 +916,8 @@ class TFin_dossier extends TObjetStd {
 				$result=$object->set_paid($user); // La facture reste en impayée pour le moment, elle passera à payée lors de l'export comptable
 			}
 			
-			$date_debut_periode = $dossier->getDateDebutPeriode($echeance,'LEASER');
-			$date_fin_periode = $dossier->getDateFinPeriode($echeance);
+			$date_debut_periode = $this->getDateDebutPeriode($echeance-1,'LEASER');
+			$date_fin_periode = $this->getDateFinPeriode($echeance-1);
 
 			$db->query("UPDATE ".MAIN_DB_PREFIX."facture_fourn SET date_debut_periode = '".date('Y-m-d',strtotime($date_debut_periode))."' , date_fin_periode = '".date('Y-m-d',strtotime($date_fin_periode))."' WHERE rowid = ".$object->id);
 			
@@ -1297,7 +1297,7 @@ class TFin_financement extends TObjetStd {
 		
 		//Dans le cas d'un financement LEASER, si la date du sole est renseignée, alors on créé les avoirs correspondant au factures fournisseur
 		//qui existe pour les échéances situées après cette date
-		if($this->type == 'LEASER' && !empty($this->date_solde)){
+		if($this->type == 'LEASER' && (!empty($this->date_solde) && $this->date_solde > 0)){
 			$dossier = new TFin_dossier;
 			$dossier->load($ATMdb, $this->fk_fin_dossier);
 			$dossier->load_factureFournisseur($ATMdb);
