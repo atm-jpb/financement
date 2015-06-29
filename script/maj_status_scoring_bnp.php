486864c64bed065c9aaf450f7e71765da038a81c
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2015-06-29 16:05:30 +0200

    ADD : avancement demande MAJ status scoring BNP

--- script/maj_status_scoring_bnp.php
+++ script/maj_status_scoring_bnp.php
@@ -5,5 +5,7 @@
 	require('../config.php');
 	require('../class/simulation.class.php');
 	
+	$PDOdb = new TPDOdb;
+	
 	$TSimulationSuivi = new TSimulationSuivi;
-	pre($TSimulationSuivi->_consulterDemandeBNP());
+	pre($TSimulationSuivi->_consulterDemandeBNP($PDOdb));
