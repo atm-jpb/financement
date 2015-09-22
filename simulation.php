99b60efb529c401c9510f6979f5ee8c897fda6e7
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2015-09-22 17:44:54 +0200

    ADD : réactivation règle si incident paiement sur dossier externe alors on affiche pas le solde dans la simulation

--- simulation.php
+++ simulation.php
@@ -860,7 +860,7 @@ function _liste_dossier(&$ATMdb, &$simulation, $mode) {
 		/*
 		 * Mise en commentaire des ancienne règle d'afficahge des soldes suite PR1504-0764 avec gestion des soldes V2
 		 */
-		//if($ATMdb->Get_field('incident_paiement')=='OUI') $dossier->display_solde = 0;
+		if($ATMdb->Get_field('incident_paiement')=='OUI' && $dossier->nature_financement == 'EXTERNE') $dossier->display_solde = 0;
 		//if($dossier->nature_financement == 'INTERNE') $dossier->display_solde = 0; // Ticket 447
 		//if($leaser->code_client == '024242') $dossier->display_solde = 0; // Ticket 447, suite
 		if($dossier->montant >= 50000 && $dossier->nature_financement == 'INTERNE') $dossier->display_solde = 0;// On ne prends que les dossiers < 50 000€ pour faire des tests
