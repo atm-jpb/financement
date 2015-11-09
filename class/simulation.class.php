5a42e665ebf6696b16dcb087df78baf2682cee86
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2015-11-09 15:47:32 +0100

    FIX date validité dans PDF simulation

--- class/simulation.class.php
+++ class/simulation.class.php
@@ -823,10 +823,10 @@ class TSimulation extends TObjetStd {
 			//$date_prochaine_echeance = $this[]
 			
 			$echeance = $d->_get_num_echeance_from_date($this->dossiers[$idDossier]['date_prochaine_echeance']);
-			$date_debut_periode = $d->getDateDebutPeriode($echeance,$type);
-			$date_fin_periode = $d->getDateFinPeriode($echeance,$type);
-			$date_debut_periode_p1 = date('Y-m-d',strtotime('+ '.$f->getiPeriode().' months', strtotime($date_debut_periode)));
-			$date_fin_periode_p1 = date('Y-m-d',strtotime('+ '.$f->getiPeriode().' months', strtotime($date_fin_periode)));
+			$date_debut_periode = $d->getDateDebutPeriode($echeance-1,$type);
+			$date_fin_periode = $d->getDateFinPeriode($echeance-1,$type);
+			$date_debut_periode_p1 = $d->getDateDebutPeriode($echeance,$type);
+			$date_fin_periode_p1 = $d->getDateFinPeriode($echeance,$type);
 			
 			/*echo $d->reference.'<br>';
 			echo $echeance.'<br>';
