8c1045bc7468d148c7dab3df8eb78b534fbc71f0
Maxime Kohlhaas <maxime@atm-consulting.fr>
2016-03-14 11:21:25 +0100

    Simulation : date démarrage vide par défaut (ticket 3887)

--- class/simulation.class.php
+++ class/simulation.class.php
@@ -62,6 +62,7 @@ class TSimulation extends TObjetStd {
 		$this->opt_mode_reglement = 'PRE';
 		$this->opt_terme = '1';
 		$this->opt_calage = '';
+		$this->date_demarrage = '';
 		$this->vr = 0;
 		$this->coeff = 0;
 		$this->fk_user_author = $user->id;
