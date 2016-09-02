160307275e589adc9ce046ed2019a4cee606e99f
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2016-09-02 11:50:28 +0200

    fix

--- script/maj_status_scoring_bnp.php
+++ script/maj_status_scoring_bnp.php
@@ -9,16 +9,17 @@
 	
 	$TSimulationSuivi = new TSimulationSuivi;
 	
-	$sql = "SELECT numero_accord_leaser 
+	$sql = "SELECT rowid, numero_accord_leaser 
 				FROM ".MAIN_DB_PREFIX."fin_simulation_suivi 
 				WHERE (fk_leaser = 3382 OR fk_leaser = 19553 OR fk_leaser = 20113)
 					AND numero_accord_leaser != '' AND numero_accord_leaser IS NOT NULL
 					AND  statut = 'WAIT' ";
 	//echo $sql;exit;
-	$PDOdb->Execute($sql);
+	$TRes = $PDOdb->ExecuteAsArray($sql);
 	
-	while($PDOdb->Get_line()){
-		$TreponseDemandes  = $TSimulationSuivi->_consulterDemandeBNP($PDOdb->Get_field('numero_accord_leaser'));
+	foreach($TRes as $res){
+		$TSimulationSuivi->load($PDOdb, $res->rowid);
+		$TreponseDemandes  = $TSimulationSuivi->_consulterDemandeBNP($res->numero_accord_leaser);
 	
 		pre($TreponseDemandes,true);
 	}
