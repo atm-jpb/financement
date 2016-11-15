bf08cee064b34cbc9fe5a8c2f2b35b4b2e3d9cdf
Maxime Kohlhaas <maxime@atm-consulting.fr>
2016-11-15 16:57:07 +0100

    Fix : simul si dossier racheté à 0, il faut vérifier si un dossier a été coché

--- class/simulation.class.php
+++ class/simulation.class.php
@@ -550,6 +550,10 @@ class TSimulation extends TObjetStd {
 		}
 		else if(empty($this->opt_no_case_to_settle)
 				&& empty($this->montant_rachete)
+				&& empty($this->dossiers_rachetes)
+				&& empty($this->dossiers_rachetes_nr)
+				&& empty($this->dossiers_rachetes_p1)
+				&& empty($this->dossiers_rachetes_nr_p1)
 				&& empty($this->montant_rachete_concurrence)) { // Soit case "aucun dossier à solder", soit choix de dossier, soit saisie d'un montant rachat concurrence
 			$this->error = 'ErrorCaseMandatory';
 			return false;
