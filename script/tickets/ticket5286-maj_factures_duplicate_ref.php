d0bb1764c6ec72d3a85205ef30e78aa52417354a
arnaud <arnaud.saint-patrice@atm-consulting.fr>
2016-12-14 16:45:29 +0100

    FIX script tk 5286

--- script/tickets/ticket5286-maj_factures_duplicate_ref.php
+++ script/tickets/ticket5286-maj_factures_duplicate_ref.php
@@ -9,22 +9,44 @@ dol_include_once('/compta/facture/class/facture.class.php');
 
 @set_time_limit(0);					// No timeout for this script
 
+$TDoubleFac = array();
+
 // On drop la view precedente si elle existe
-$atmdb = new TPDOdb; $sql = "DROP VIEW facture_view_5286"; 
-$atmdb->Execute($sql);
+$PDOdb = new TPDOdb;
+$sql = "DROP VIEW facture_view_5286"; 
+$PDOdb->Execute($sql);
 
 // On recréé la view
 $sql = "CREATE VIEW facture_view_5286 AS SELECT * from llx_facture GROUP BY facnumber HAVING COUNT(facnumber) > 1";
-$atmdb->Execute($sql);
+$PDOdb->Execute($sql);
 
 $nbFact = 0;
+// On ressort toutes les factures en doublon
+$sql = "SELECT * FROM facture_view_5286";
 
-while($ATMdb->Get_line()){
+while($object = $PDOdb->fetch($sql)){
 	$nbFact++;
-	$facture = new Facture($db);
-	$idfac = 
-	$facture->fetch($idfac);
-	echo '<br><br>';
+	$TDoubleFac[] = $object;
 }
 
-echo $nbFact.' ****** en doublon ';
+if(!empty($TDoubleFac)) {
+	foreach($TDoubleFac as $goodFacture) {
+		//var_dump($goodFacture);exit;
+		$sql = "SELECT rowid from llx_facture WHERE facnumber = '".$goodFacture->facnumber."' AND entity <> ".$goodFacture->entity;
+		delete_doublons_from_sql($sql,$PDOdb);
+	}
+}
+
+echo '****** '.$nbFact.' factures ****** en doublon supprimées ******';
+
+
+// Fonction qui recupere le sql des doublons et supprimes toutes les factures qu'elle trouve
+function delete_doublons_from_sql($sql = null, $PDOdb){
+	global $db;
+	
+	while($badFacture = $PDOdb->fetch($sql)){
+		$facture = new Facture($db);
+		$facture->fetch($badFacture->rowid);
+		//$facture->delete();
+	}
+}
