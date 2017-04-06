53a9b01e806245cd5701821a212e408ca9d218a7
Maxime Kohlhaas <maxime@atm-consulting.fr>
2017-04-06 11:57:18 +0200

    Avec les tables SQL c'est mieux

---
+++ sql/llx_c_financement_conf_solde.sql
@@ -0,0 +1,10 @@
+CREATE TABLE llx_c_financement_conf_solde
+(
+  rowid             integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  fk_type_contrat   varchar(255) NOT NULL,
+  periode           integer NOT NULL,
+  base_solde        varchar(20) NOT NULL,
+  percent	    double(24,8) NOT NULL DEFAULT 0,
+  entity            integer NOT NULL DEFAULT 1,
+  active            tinyint NOT NULL DEFAULT 1		
+)ENGINE=innodb;
