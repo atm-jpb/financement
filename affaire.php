3fc033d590f2c0c1258fe2a21124d3c9fa06d6f6
Maxime Kohlhaas <maxime@atm-consulting.fr>
2017-08-29 15:19:30 +0200

    Filtre société par entité affaire

--- affaire.php
+++ affaire.php
@@ -396,7 +396,7 @@ function _fiche(&$ATMdb, &$affaire, $mode) {
 		
 		$sql = "SELECT rowid, nom 
 				FROM ".MAIN_DB_PREFIX."societe s
-				WHERE 1";
+				WHERE entity = ".$affaire->entity;
 	//	print $sql;
 		$Tab = TRequeteCore::_get_id_by_sql($ATMdb, $sql,'nom', 'rowid');
 		
