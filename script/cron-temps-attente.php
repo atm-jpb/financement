8488e63e9e371fec2f39be0a6fe47f8caab0b0f1
atm-greg <gregory.blemand@atm-consulting.fr>
2018-01-31 11:58:23 +0100

    script MAJ attente non-cloturées

---
+++ script/cron-temps-attente.php
@@ -0,0 +1,31 @@
+<?php
+set_time_limit(0);
+ini_set('memory_limit', '1024M');
+
+define('INC_FROM_CRON_SCRIPT', true);
+
+require_once __DIR__.'/../config.php';
+
+dol_include_once('/financement/class/simulation.class.php');
+dol_include_once('/financement/class/affaire.class.php');
+dol_include_once('/financement/class/dossier.class.php');
+dol_include_once('/financement/class/score.class.php');
+dol_include_once('/financement/class/grille.class.php');
+
+$ATMdb = new TPDOdb;
+
+$sql = "SELECT rowid FROM " . MAIN_DB_PREFIX . "fin_simulation";
+$sql.= " WHERE 1=1 ";
+$sql.= " AND accord NOT IN ('OK', 'KO', 'SS') ";
+$sql.= " ORDER BY date_simul DESC";
+$res = $db->query($sql);
+
+while($obj = $db->fetch_object($res)){
+    
+    $simulation = new TSimulation();
+    $simulation->load($ATMdb, $db, $obj->rowid);
+    
+    $simulation->get_attente($ATMdb);
+    echo $obj->rowid.'<br>';
+}
+
