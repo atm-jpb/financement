077e0cdf9a8e0e70164de2798334ae9ccb2c04f9
Maxime Kohlhaas <maxime@atm-consulting.fr>
2018-03-06 14:36:20 +0100

    Fix timestamp in XML headers

--- class/wse-php/WSSESoap.php
+++ class/wse-php/WSSESoap.php
@@ -130,7 +130,7 @@ class WSSESoap
 
         $timestamp = $this->soapDoc->createElementNS(self::WSUNS, self::WSUPFX.':Timestamp');
         $security->insertBefore($timestamp, $security->firstChild);
-        $currentTime = strtotime('-1 hour');
+        $currentTime = time();
         $created = $this->soapDoc->createElementNS(self::WSUNS,  self::WSUPFX.':Created', date("Y-m-d\TH:i:s\Z", $currentTime));
         $timestamp->appendChild($created);
         if (!is_null($secondsToExpire)) {
