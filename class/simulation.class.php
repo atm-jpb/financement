9c50bce4f522908b78a528d1064ac99b77ce67a9
Maxime Kohlhaas <maxime@atm-consulting.fr>
2018-06-21 15:40:41 +0200

    Fix simul : règle qui impose de cocher une case si pas de dossier à solder

--- class/simulation.class.php
+++ class/simulation.class.php
@@ -703,6 +703,8 @@ class TSimulation extends TObjetStd {
 		$this->montant_total_finance = $this->montant + $this->montant_rachete + $this->montant_rachete_concurrence;
 		// Changement du 13.09.02 : le montant renseigné comportera déjà le montant des rachats
 		$this->montant_total_finance = $this->montant;
+		
+		$TDossSelected = $this->_getDossierSelected();
 
 		if(empty($this->fk_type_contrat)) { // Type de contrat obligatoire
 			$this->error = 'ErrorNoTypeContratSelected';
@@ -726,10 +728,7 @@ class TSimulation extends TObjetStd {
 		}
 		else if(empty($this->opt_no_case_to_settle)
 				&& empty($this->montant_rachete)
-				&& empty($this->dossiers_rachetes)
-				&& empty($this->dossiers_rachetes_nr)
-				&& empty($this->dossiers_rachetes_p1)
-				&& empty($this->dossiers_rachetes_nr_p1)
+				&& empty($TDossSelected)
 				&& empty($this->montant_rachete_concurrence)) { // Soit case "aucun dossier à solder", soit choix de dossier, soit saisie d'un montant rachat concurrence
 			$this->error = 'ErrorCaseMandatory';
 			return false;
