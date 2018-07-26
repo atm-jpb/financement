029c84abbb1cda8189345a28b30297f851ffd898
Maxime Kohlhaas <maxime@atm-consulting.fr>
2018-07-26 16:33:46 +0200

    CAPEA : suite script import

--- script/import_capea/import_dossiers_capea.php
+++ script/import_capea/import_dossiers_capea.php
@@ -28,8 +28,7 @@ while($line = fgetcsv($f,2048,';','"')) {
 	if($i>5) break;
 }
 unset($TData[0]);
-pre($TData,true);
-exit;
+
 $i = $crea = 0;
 foreach ($TData as $datadoss) {
 	$crea += createDossier($PDOdb, $datadoss);
@@ -53,45 +52,55 @@ function parseline(&$PDOdb, &$TData, $line) {
 	if(!empty($line[4])) $data['financement']['fk_soc_client'] = getSocieteBySIREN($PDOdb, $line[4]);
 	
 	if(empty($data['financement']['fk_soc_client'])) {
-		echo $data['financementLeaser']['reference'].' - SIREN non trouvé : '.$line[4].'<br>';
+		echo '<hr>'.$data['financementLeaser']['reference'].' - SIREN non trouvé : '.$line[4];
 		return 0;
 	}
 	
 	if(!empty($line[8])) $data['financementLeaser']['date_debut'] = $line[8];
 	if(!empty($line[9])) $data['financementLeaser']['date_fin'] = $line[9];
 	if(!empty($line[10])) $data['financementLeaser']['periodicite'] = $TPeriodicite[$line[10]];
-	if(!empty($line[11])) $data['financementLeaser']['duree'] = $line[11];
-	if(!empty($line[12])) $data['financementLeaser']['montant'] = $line[12];
-	if(!empty($line[13])) $data['financementLeaser']['echeance'] = $line[13];
-	if(!empty($line[14])) $data['financementLeaser']['loyer_intercalaire'] = $line[14];
+	if(!empty($line[11])) $data['financementLeaser']['duree'] = price2num($line[11]);
+	if(!empty($line[12])) $data['financementLeaser']['montant'] = price2num($line[12]);
+	if(!empty($line[13])) $data['financementLeaser']['echeance'] = price2num($line[13]);
+	if(!empty($line[14])) $data['financementLeaser']['loyer_intercalaire'] = price2num($line[14]);
 	
-	return $data;
+	$TData[] = $data;
 }
 
 // Création de l'affaire, du dossier et des financements
 function createDossier(&$PDOdb, $data) {
 	echo '<hr>';
-	
 	if(empty($data['financementLeaser']['reference'])) {
 		echo $data['financementLeaser']['reference'].' - Référence vide';
 		return 0;
 	}
 	pre($data,true);
-	return;
 	
 	$aff = new TFin_affaire();
-	$aff->reference = '';
-	$aff->date_affaire = '';
+	$aff->nature_financement= 'INTERNE';
+	$aff->reference = $data['financementLeaser']['reference'];
+	$aff->set_date('date_affaire', $data['financementLeaser']['date_debut']);
+	$aff->montant = $data['financementLeaser']['montant'];
+	$aff->contrat = 'INTEGRAL';
+	$aff->type_financement = 'ADOSSEE';
+	$aff->fk_soc = $data['financement']['fk_soc_client'];
+	$aff->entity = 12;
 	$aff->save($PDOdb);
 	
 	$doss = new TFin_dossier();
-	$doss->financement->set_values($data['financement']);
+	$doss->financement->set_values($data['financementLeaser']);
 	$doss->financementLeaser->set_values($data['financementLeaser']);
+	$doss->entity = 12;
 	$doss->save($PDOdb);
+	
+	$doss->financement->setProchaineEcheanceClient($PDOdb, $doss);
+	$doss->financementLeaser->setProchaineEcheanceClient($PDOdb, $doss);
 	
 	$doss->addAffaire($PDOdb, $aff->getId());
 	
-	echo $data['financementLeaser']['reference'].' - Dossier créé<br>';
+	$doss->save($PDOdb);
+	
+	echo $data['financementLeaser']['reference'].' - Dossier créé';
 	
 	return 1;
 }
@@ -101,9 +110,8 @@ function getSocieteBySIREN(&$PDOdb, $siren) {
 	if(empty($siren)) return 0;
 	
 	$sql = 'SELECT rowid FROM '.MAIN_DB_PREFIX.'societe WHERE entity = 12 AND siren = \''.substr($siren, 0,9).'\'';
-	echo $sql;
 	$Tab = $PDOdb->ExecuteAsArray($sql);
-	if(!empty($Tab)) return $Tab[0];
+	if(!empty($Tab)) return $Tab[0]->rowid;
 	
 	return 0;
 }
