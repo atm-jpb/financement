9329b045fb041f7a23a0c22dc68c448064308bbc
Maxime Kohlhaas <maxime@atm-consulting.fr>
2018-10-04 10:10:02 +0200

    Fin script calcul echeance

--- script/cron-calcul-echeance.php
+++ script/cron-calcul-echeance.php
@@ -21,6 +21,7 @@ $sql.= "WHERE (fin.date_solde < '1000-01-01' OR fin.date_solde IS NULL) ";
 $sql.= "AND fin.date_prochaine_echeance <= '".date('Y-m-d')."' ";
 $sql.= "AND fin.reference != '' ";
 $sql.= "AND fin.date_debut > '1000-01-01' ";
+//$sql.= "AND fin.type = 'CLIENT' ";
 //$sql.= "AND fin.rowid = 97378";
 
 echo $sql.'<hr>';
@@ -32,16 +33,7 @@ foreach ($TRowid as $obj) {
 	echo $obj->rowid.' - '. $obj->fk_fin_dossier .'<br>';
 	$fin = new TFin_financement();
 	$fin->load($PDOdb, $obj->rowid);
-	
-	$dossier = new TFin_dossier();
-	$dossier->load($PDOdb, $obj->fk_fin_dossier,false);
-	$echeance = $dossier->_get_num_echeance_from_date(time());
-	$echeance = $echeance +2;
-	//echo $echeance.'<br>';
-	$fin->numero_prochaine_echeance = $echeance;
-	//echo $dossier->getDateDebutPeriode($echeance);
-	echo 'ECH : '.$echeance.' - DATE : '.$dossier->getDateDebutPeriode($echeance-1,'LEASER').'<br>';
-	$fin->set_date('date_prochaine_echeance', $dossier->getDateDebutPeriode($echeance-1,'LEASER'));
+	$fin->setEcheanceExterne();
 	$fin->save($PDOdb);
 }
 
