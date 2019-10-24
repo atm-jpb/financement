eff15c2642a7ad1f2ca46da04ad4916a3c93dadc
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2019-10-24 09:35:40 +0200

    FIX : Fichier scoring_server utilisé uniquement par Lixxbail

--- script/webservice/scoring_server.php
+++ script/webservice/scoring_server.php
@@ -190,21 +190,9 @@ function repondreDemande($authentication, $TReponse)
 							
 							$found = false;
                             foreach($simulation->TSimulationSuivi as $simulationSuivi) {
-                                if($simulationSuivi->statut_demande == 2) {
+                                if($simulationSuivi->leaser->array_options['options_edi_leaser'] == $fuser->array_options['options_fk_leaser_webservice']) {
                                     $found = true;
                                     break;
-                                }
-                            }
-
-                            if(! $found) {
-                                foreach ($simulation->TSimulationSuivi as &$simulationSuivi)
-                                {
-                                    if ($simulationSuivi->leaser->array_options['options_edi_leaser'] == 'CMCIC')
-                                        //					if ($simulationSuivi->leaser->array_options['options_edi_leaser'] == $fuser->array_options['options_fk_leaser_webservice'])
-                                    {
-                                        $found = true;
-                                        break;
-                                    }
                                 }
                             }
 							
