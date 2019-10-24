feda1b13e9b0a8a401bf353883d1e2676e4cb6b6
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2019-10-24 10:51:11 +0200

    NEW : Conformite, add leaser column in list

--- conformite/list.php
+++ conformite/list.php
@@ -30,6 +30,7 @@ $search_ref = GETPOST('search_ref');
 $search_entity = GETPOST('search_entity');
 if(! empty($search_entity) && ! is_array($search_entity)) $search_entity = explode(',', $search_entity);
 $search_thirdparty = GETPOST('search_thirdparty');
+$search_leaser = GETPOST('search_leaser');
 $search_status = GETPOST('search_status');
 
 $action = GETPOST('action');
@@ -53,21 +54,23 @@ foreach($dao->entities as $mc_entity) $TEntity[$mc_entity->id] = $mc_entity->lab
  */
 // Remove filters
 if(GETPOST('button_removefilter_x','alpha') || GETPOST('button_removefilter.x','alpha') || GETPOST('button_removefilter','alpha')) {
-    unset($search_ref, $search_entity, $search_thirdparty, $search_status);
+    unset($search_ref, $search_entity, $search_thirdparty, $search_leaser, $search_status);
 }
 
-$sql = 'SELECT s.rowid, s.reference, s.rowid, soc.rowid as fk_soc, c.status, s.entity, c.fk_user, c.rowid as fk_conformite, c.commentaire, c.date_cre';
+$sql = 'SELECT s.rowid, s.reference, soc.rowid as fk_soc, c.status, s.entity, c.fk_user, c.rowid as fk_conformite, c.commentaire, c.date_cre, lea.rowid as fk_leaser';
 $sql.= ' FROM '.MAIN_DB_PREFIX.'fin_conformite c';
 $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'fin_simulation s ON (c.fk_simulation = s.rowid)';
 $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'societe soc ON (s.fk_soc = soc.rowid)';
+$sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'societe lea ON (s.fk_leaser = lea.rowid)';
 $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'user u ON (c.fk_user = u.rowid)';
 
 $strEntityShared = getEntity('fin_simulation', true);
 $TEntityShared = explode(',', $strEntityShared);
-//var_dump($search_entity);
+
 $sql.= ' WHERE 1';
 if(! empty($search_ref)) $sql .= natural_search('s.reference', $search_ref);
 if(! empty($search_thirdparty)) $sql .= natural_search('soc.nom', $search_thirdparty);
+if(! empty($search_leaser)) $sql .= natural_search('lea.nom', $search_leaser);
 if(! empty($search_status) && $search_status != -1) $sql .= natural_search('c.status', $search_status);
 if(! empty($search_entity)) {
     $TSearchEntity = array_intersect($TEntityShared, $search_entity);
@@ -105,6 +108,7 @@ if($limit > 0 && $limit != $conf->liste_limit) $param .= '&limit='.$limit;
 if(! empty($search_ref)) $param .= '&search_ref='.urlencode($search_ref);
 if(! empty($search_entity)) $param .= '&search_entity='.urlencode(implode(',', $search_entity));
 if(! empty($search_thirdparty)) $param .= '&search_thirdparty='.urlencode($search_thirdparty);
+if(! empty($search_leaser)) $param .= '&search_leaser='.urlencode($search_leaser);
 if(! empty($search_status)) $param .= '&search_status='.urlencode($search_status);
 
 print '<form method="GET" action="'.$_SERVER['PHP_SELF'].'" name="formfilter">';
@@ -125,7 +129,7 @@ print '<table class="tagtable liste">';
 print '<tr class="liste_titre">';
 
 // Entity
-print '<td colspan="8" style="min-width: 150px;">';
+print '<td colspan="9" style="min-width: 150px;">';
 print '<span>'.$langs->trans('DemandReasonTypeSRC_PARTNER').' : </span>';
 print Form::multiselectarray('search_entity', $TEntity, $search_entity, 0, 0, 'style="min-width: 250px;"');
 print '</td>';
@@ -144,6 +148,11 @@ print '<td></td>';
 // Thirdparty
 print '<td>';
 print '<input type="text" name="search_thirdparty" value="'.$search_thirdparty.'" size="20" />';
+print '</td>';
+
+// Leaser
+print '<td>';
+print '<input type="text" name="search_leaser" value="'.$search_leaser.'" size="20" />';
 print '</td>';
 
 // Statut
@@ -174,6 +183,7 @@ print '<tr class="liste_titre">';
 print_liste_field_titre('Ref.', $_SERVER['PHP_SELF'], 's.reference', '', $param, 'style="text-align: left;"', $sortfield, $sortorder);   // Ref simulation
 print_liste_field_titre('Partenaire', $_SERVER['PHP_SELF'], 's.entity', '', $param, 'style="text-align: left;"', $sortfield, $sortorder);   // Entity
 print_liste_field_titre('Client', $_SERVER['PHP_SELF'], 's.fk_soc', '', $param, 'style="text-align: left;"', $sortfield, $sortorder);   // Thirdparty
+print_liste_field_titre('Leaser', $_SERVER['PHP_SELF'], 's.fk_leaser', '', $param, 'style="text-align: left;"', $sortfield, $sortorder);   // Leaser
 print_liste_field_titre('Statut', $_SERVER['PHP_SELF'], 'c.status', '', $param, 'style="text-align: left;"', $sortfield, $sortorder);   // Statut simul
 print_liste_field_titre($langs->trans('DateCreation'), $_SERVER['PHP_SELF'], 'c.date_cre', '', $param, 'style="text-align: left;"', $sortfield, $sortorder);   // Statut simul
 print_liste_field_titre($langs->trans('User'), $_SERVER['PHP_SELF'], 'u.login', '', $param, 'style="text-align: left;"', $sortfield, $sortorder);   // Statut simul
@@ -217,6 +227,11 @@ for($i = 0 ; $i < min($num, $limit) ; $i++) {
     print $form->textwithtooltip($soc->getNomUrl(1, '', 18), $soc->name);
     print '</td>';
 
+    // Leaser
+    print '<td>';
+    print $form->textwithtooltip($leaser->getNomUrl(1, '', 18), $leaser->name);
+    print '</td>';
+
     // Statut
     print '<td>';
     print $langs->trans(Conformite::$TStatus[$obj->status]);
@@ -245,82 +260,3 @@ print '</table></div>';
 print '</form>';
 
 llxFooter();
-
-function print_attente($compteur) {
-    global $conf;
-
-    $hour = intval($compteur / 3600);
-    $min = ($compteur % 3600) / 60;
-    if(! empty($conf->global->FINANCEMENT_FIRST_WAIT_ALARM) && $min >= (int) $conf->global->FINANCEMENT_FIRST_WAIT_ALARM) $style = 'color: orange;';
-    if(! empty($conf->global->FINANCEMENT_SECOND_WAIT_ALARM) && $min >= (int) $conf->global->FINANCEMENT_SECOND_WAIT_ALARM) $style = 'color: red;';
-
-    $ret = sprintf("%'.02dh%'.02d", $hour, $min);   // Format to 00h00
-
-    if(! empty($style)) $ret = '<span style="'.$style.'">'.$ret.'</span>';
-
-    return $ret;
-}
-
-function getStatutSuivi($idSimulation, $statut, $fk_fin_dossier, $nb_ok, $nb_refus, $nb_wait, $nb_err) {
-    global $langs, $db;
-    if(! function_exists('get_picto')) dol_include_once('/financement/lib/financement.lib.php');
-
-    $suivi_leaser = '';
-    $PDOdb = new TPDOdb;
-    $s = new TSimulation;
-    $s->load($PDOdb, $idSimulation, false);
-
-    $iconSize = 'font-size: 21px;';
-    if($s->fk_action_manuelle > 0) {
-        $title = '';
-        $color = 'deeppink';
-        if($s->fk_action_manuelle == 2) $color = 'green';
-        $sql = 'SELECT label FROM '.MAIN_DB_PREFIX.'c_financement_action_manuelle WHERE rowid = '.$s->fk_action_manuelle;
-        $resql = $db->query($sql);
-
-        if($obj = $db->fetch_object($resql)) {
-            $title = $langs->trans($obj->label);
-        }
-
-        $suivi_leaser .= get_picto('manual', $title, $color);
-
-        $db->free($resql);
-    }
-    else {
-        $suivi_leaser .= '<a href="'.dol_buildpath('/financement/simulation/simulation.php?id='.$idSimulation, 1).'#suivi_leaser">';
-
-        if(! empty($fk_fin_dossier)) { // La simulation a été financée, lien direct vers le dossier
-            $suivi_leaser = '<a href="'.dol_buildpath('/financement/dossier.php?id='.$fk_fin_dossier, 1).'">';
-            $suivi_leaser .= get_picto('money');
-            $suivi_leaser .= '</a>';
-        }
-        else if($statut == 'OK') $suivi_leaser .= get_picto('super_ok');
-        else if($statut == 'WAIT_SELLER') $suivi_leaser .= get_picto('wait_seller');
-        else if($statut == 'WAIT_LEASER') $suivi_leaser .= get_picto('wait_leaser');
-        else if($statut == 'WAIT_AP') $suivi_leaser .= get_picto('wait_ap');
-        else if($nb_ok > 0) $suivi_leaser .= get_picto('ok');
-        else if($nb_refus > 0) $suivi_leaser .= get_picto('refus');
-        else if($nb_wait > 0) $suivi_leaser .= get_picto('wait');
-        else if($nb_err > 0) $suivi_leaser .= get_picto('err');
-        else $suivi_leaser .= '';//'<img title="'.$langs->trans('Etude').'" src="'.dol_buildpath('/financement/img/WAIT.png',1).'" />';
-        $suivi_leaser .= '</a>';
-    }
-
-    $suivi_leaser .= ' <span style="color: #00AA00;">'.$nb_ok.'</span>';
-    $suivi_leaser .= ' <span style="color: #FF0000;">'.$nb_refus.'</span>';
-    $suivi_leaser .= ' <span>'.($nb_ok + $nb_refus + $nb_wait + $nb_err).'</span>';
-
-    return $suivi_leaser;
-}
-
-function _simu_edit_link($simulId, $date) {
-    if(! function_exists('get_picto')) dol_include_once('/financement/lib/financement.lib.php');
-
-    if(strtotime($date) > dol_now()) {
-        $return = '<a href="'.dol_buildpath('/financement/simulation/simulation.php', 1).'?id='.$simulId.'&action=edit">'.get_picto('edit').'</a>';
-    }
-    else {
-        $return = '';
-    }
-    return $return;
-}
