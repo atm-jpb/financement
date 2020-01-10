9bd7cdf5097fcb02d36fe1f2975dd53aa13d8a3b
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-01-10 10:37:38 +0100

    FIX : Modification du script pour delete des doublons

--- script/delete_dossier_doublon.php
+++ script/delete_dossier_doublon.php
@@ -13,21 +13,21 @@ $debug = GETPOST('debug', 'int');
 $limit = GETPOST('limit', 'int');
 $commit = GETPOST('commit', 'int');
 
-// On va chercher toutes les références de dossiers en doublons dans les entités 3 et 10
+// On va chercher toutes les références de dossiers en doublons dans les entités 20, 21, 22 et 24
 $subquery = 'SELECT df.reference';
 $subquery.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier_financement df';
 $subquery.= ' LEFT JOIN '.MAIN_DB_PREFIX.'fin_dossier d ON (d.rowid = df.fk_fin_dossier)';
-$subquery.= ' WHERE d.entity IN (3,10)';
+$subquery.= ' WHERE d.entity IN (20,21,22,24)';
 $subquery.= " AND df.type = 'LEASER'";
 $subquery.= " AND df.reference <> ''";
 $subquery.= ' GROUP BY df.reference';
 $subquery.= ' HAVING COUNT(*) > 1';
 
-// On récupère que les 'fk_fin_dossier' des dossiers de l'entité 3 pour les delete
+// On récupère que les 'fk_fin_dossier' des dossiers des entités 21, 22 et 24 pour les delete
 $sql = 'SELECT df.fk_fin_dossier';
 $sql.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier_financement df';
 $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'fin_dossier d ON (d.rowid = df.fk_fin_dossier)';
-$sql.= ' WHERE d.entity = 3';
+$sql.= ' WHERE d.entity <> 20';
 $sql.= ' AND df.reference IN ('.$subquery.')';
 if(! empty($limit)) $sql.= ' LIMIT '.$limit;
 
