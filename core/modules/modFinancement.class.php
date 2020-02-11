bd67516b25e09dfc88a5f1329eff1939004941fe
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-02-11 11:16:44 +0100

    FIX : Extrafields, set entity to 0

--- core/modules/modFinancement.class.php
+++ core/modules/modFinancement.class.php
@@ -1116,23 +1116,23 @@ class modFinancement extends DolibarrModules
 		
 		dol_include_once('/core/class/extrafields.class.php');
 		$extra = new ExtraFields($db);
-		$extra->addExtraField('fk_leaser_webservice', 'Identifiant du leaser associé pour les réponses de demande de financement', 'int', '1', '', 'user', 0, 0, '', unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
+		$extra->addExtraField('fk_leaser_webservice', 'Identifiant du leaser associé pour les réponses de demande de financement', 'int', '1', '', 'user', 0, 0, '', unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
 		$extra->addExtraField('cmcic_apporteur_id', 'Apporteur ID', 'varchar', 10, 255, 'entity');
 		
 		
-		$extra->addExtraField('percent_duree_vie', '% durée de vie', 'double', 15, '24,8', 'entity', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('percent_moyenne_intercalaire', '% moyenne intercalaire', 'double', 20, '24,8', 'entity', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
+		$extra->addExtraField('percent_duree_vie', '% durée de vie', 'double', 15, '24,8', 'entity', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('percent_moyenne_intercalaire', '% moyenne intercalaire', 'double', 20, '24,8', 'entity', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
 		
-		$extra->addExtraField('percent_surfactplus', '% surfact+', 'double', 10, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('percent_commission', '% commission', 'double', 15, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('percent_intercalaire', '% intercalaire', 'double', 20, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('percent_prime_volume', '% prime volume', 'double', 25, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('percent_relocation', '% relocation', 'double', 27, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('bonus_renta', 'Bonus renta', 'double', 30, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('non_cape_lrd', 'Soldes non capés LRD', 'boolean', 35, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
+		$extra->addExtraField('percent_surfactplus', '% surfact+', 'double', 10, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('percent_commission', '% commission', 'double', 15, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('percent_intercalaire', '% intercalaire', 'double', 20, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('percent_prime_volume', '% prime volume', 'double', 25, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('percent_relocation', '% relocation', 'double', 27, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('bonus_renta', 'Bonus renta', 'double', 30, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('non_cape_lrd', 'Soldes non capés LRD', 'boolean', 35, '24,8', 'societe', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
 
-		$extra->addExtraField('date_debut_periode', 'Date début période', 'date', 10, '', 'facture_fourn', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
-		$extra->addExtraField('date_fin_periode', 'Date fin période', 'date', 20, '', 'facture_fourn', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1);
+		$extra->addExtraField('date_debut_periode', 'Date début période', 'date', 10, '', 'facture_fourn', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
+		$extra->addExtraField('date_fin_periode', 'Date fin période', 'date', 20, '', 'facture_fourn', 0, 0, 0, unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'), 1, '', '-1', '', '', 0);
 		
 		return $this->_init($sql, $options);
 	}
