c3be69c25d6650e4a63b7f9d415544063ca49d91
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-02-13 15:23:54 +0100

    NEW : Script

--- script/delete_dossier.php
+++ script/delete_dossier.php
@@ -1,36 +1,80 @@
 <?php
 
-require_once('../config.php');
+set_time_limit(0);
+
+require_once '../config.php';
 dol_include_once('/financement/class/dossier.class.php');
+dol_include_once('/financement/class/dossierRachete.class.php');
 dol_include_once('/financement/class/affaire.class.php');
 dol_include_once('/financement/class/grille.class.php');
 
-$limit = GETPOST('limit', 'int');
-
-$sql = 'SELECT rowid, fk_fin_dossier';
-$sql.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier_financement';
-$sql.= " WHERE reference LIKE 'DELETE_%'";
-if(! empty($limit)) $sql.= ' LIMIT '.$limit;
-
-$resql = $db->query($sql);
-if(! $resql) {
-	dol_print_error($db);
-	exit;
-}
+$action = GETPOST('action', 'alpha');
 
 $PDOdb = new TPDOdb;
-$nb = $db->num_rows($resql);
 
-while($obj = $db->fetch_object($resql)) {
-    $dossier = new TFin_dossier;
-    $dossier->load($PDOdb, $obj->fk_fin_dossier);
+llxHeader();
 
-    $affaire = new TFin_affaire;
-    $affaire->load($PDOdb, $dossier->TLien[0]->fk_fin_affaire);
-    $affaire->delete($PDOdb);
+if($action == 'import' && substr($_FILES['fileToImport']['name'], -4) === '.csv') {
+    $f = fopen($_FILES['fileToImport']['tmp_name'], 'r');
+    $i = $nbDeleted = 0;
 
-    $dossier->delete($PDOdb, true, false, false);
+    while ($TLine = fgetcsv($f, 2048, ';', '"')) {
+        $i++;
+        if ($i > 1) {
+            $nbDeleted += deleteContrat($PDOdb, $TLine);
+        }
+    }
+    fclose($f);
 }
-$db->free($resql);
+
 ?>
-<span>Nb to be deleted : <?php echo $nb; ?>
+    <h3>Solder des dossiers</h3>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="action" value="import" />
+        <table>
+            <tr>
+                <td style="width: 130px;"><span>Fichier CSV : </span></td>
+                <td><input type="file" name="fileToImport" /></td>
+            </tr>
+        </table>
+        <br/><br/>
+        <input class="butAction" type="submit" name="submit" value="Importer" />
+        <input class="butActionDelete" type="reset" name="reset" value="Annuler" />
+    </form>
+    <br/>
+    <div id="retours">
+    </div>
+
+<?php
+
+llxFooter();
+
+function deleteContrat(TPDOdb &$PDOdb, $TLine) {
+    $TIndex = array(
+        'ref_contrat' => 1
+    );
+
+    $reference = trim($TLine[$TIndex['ref_contrat']]);
+
+    $d = new TFin_dossier;
+    $res = $d->loadReference($PDOdb, $reference, false, 1);
+
+    if($res !== false) {
+        if(! DossierRachete::isDossierSelected($d->rowid)) {
+//            $d->delete($PDOdb, true, false, false);
+            return 1;
+        }
+        else {
+            ?>
+            <script type="text/javascript">
+                $(document).ready(function() {
+                    $('div#retours').append('<p><?php echo $reference; ?></p>');
+                });
+            </script>
+            <?php
+            return 0;
+        }
+    }
+
+    return 0;
+}
