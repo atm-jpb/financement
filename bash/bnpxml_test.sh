a2146e63bdb8c8fb4b43e17757093fb1bcd45cc6
Maxime Kohlhaas <maxime@atm-consulting.fr>
2020-03-04 09:13:36 +0100

    Fix chemin bash suite nouveau serveur

--- bash/bnpxml_test.sh
+++ bash/bnpxml_test.sh
@@ -1,3 +1,3 @@
-echo 'cd in \n put '$1'\n' > /var/www/dolibarr-fin/documents/financement/XML/BNP/bnpcmd.txt
+echo 'cd in \n put '$1'\n' > /var/www/html/dolibarr-fin/documents/financement/XML/BNP/bnpcmd.txt
 
-sftp -b /var/www/dolibarr-fin/documents/financement/XML/BNP/bnpcmd.txt -P 6710 G04920QY@159.50.103.15
+sftp -b /var/www/html/dolibarr-fin/documents/financement/XML/BNP/bnpcmd.txt -P 6710 G04920QY@159.50.103.15
