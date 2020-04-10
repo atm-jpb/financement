c14b953b1217443a33e5f1308450da909f2a9c4d
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-04-10 16:22:26 +0200

    NEW : Script to update all dossiers rates

---
+++ script/updateDossierTaux.php
@@ -0,0 +1,57 @@
+<?php
+
+ini_set('max_execution_time', 0);
+require_once '../config.php';
+dol_include_once('/financement/class/dossier.class.php');
+dol_include_once('/financement/class/grille.class.php');
+
+$PDOdb = new TPDOdb;
+$limit = GETPOST('limit', 'int');
+$commit = GETPOST('commit', 'int');
+$fk_dossier = GETPOST('fk_dossier', 'int');
+
+$sql = 'SELECT fk_fin_dossier';
+$sql.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier_financement';
+$sql.= " WHERE type = 'LEASER'";
+$sql.= " AND (date_solde is null or date_solde < '1970-01-01')";
+$sql.= ' AND montant_solde = 0';
+$sql.= ' AND date_fin >= NOW()';
+if(! empty($fk_dossier)) $sql.= ' AND fk_fin_dossier = '.$db->escape($fk_dossier);
+
+if(! empty($limit)) $sql.= ' LIMIT '.$limit;
+
+$resql = $db->query($sql);
+if(! $resql) {
+	dol_print_error($db);
+	exit;
+}
+
+$nbLine = $db->num_rows($resql);
+print '<span>Nb Lines : '.$nbLine.'</span>';
+
+//if(empty($commit)) exit;
+
+while($obj = $db->fetch_object($resql)) {
+    var_dump($obj->fk_fin_dossier);
+
+    $d = new TFin_dossier;
+    $d->load($PDOdb, $obj->fk_fin_dossier, false);
+
+    $TFin = array('financementLeaser');
+    if($d->nature_financement == 'INTERNE') $TFin[] = 'financement';
+
+    foreach($TFin as $finKey) {
+        /** @var TFin_financement $f */
+        $f = &$d->$finKey;
+
+        $f->calculTaux();
+        var_dump($f->type, $f->taux);
+    }
+//    $d->save($PDOdb);
+//    break;
+}
+$db->free($resql);
+
+?>
+<br/>
+<span>OK !</span>
