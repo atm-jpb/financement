688206153011dfaccb517ec7fdf6055c474fc9d3
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-06-24 10:57:46 +0200

    FIX : Gestion des lots de dossiers

--- script/gestionMoratoires.php
+++ script/gestionMoratoires.php
@@ -88,6 +88,7 @@ function getUsefulData($TLine) {
 /**
  * @param   TPDOdb $PDOdb
  * @param   array  $TData
+ * @param   array  $TError
  * @return  int
  */
 function updateDossierSolde(TPDOdb $PDOdb, $TData, &$TError) {
@@ -98,95 +99,111 @@ function updateDossierSolde(TPDOdb $PDOdb, $TData, &$TError) {
 
     foreach($TData as $data) {
         $d = new TFin_dossier;
-        $d->loadReference($PDOdb, $data['reference'], false, $data['entity']);
-        if(empty($d->rowid)) {  // Failed to load
-            $TError[] = $data;
-            continue;
-        }
-        $d->load_affaire($PDOdb);
 
-        // On duplique le contrat
-        $newDossier = new TFin_dossier;
-        $newDossier->load($PDOdb, $d->rowid, false);
-        $newDossier->load_affaire($PDOdb);
-
-        // On unset les id pour en créer des nouveaux
-        unset($newDossier->id, $newDossier->rowid);
-        unset($newDossier->financement->id, $newDossier->financement->rowid, $newDossier->financement->reference);
-        unset($newDossier->financementLeaser->id, $newDossier->financementLeaser->rowid, $newDossier->financementLeaser->reference);
-
-        $newDossier->save($PDOdb);  // Saving to get Id
-
-        // Update references
-        $newDossier->financement->reference = $d->financement->reference.'-COVID';
-        $newDossier->financementLeaser->reference = $d->financementLeaser->reference.'-COVID';
-
-        // On solde le dossier
-        $newDossier->financement->montant_solde = $newDossier->financement->reste;
-        $newDossier->financement->date_solde = strtotime('2020-03-31');
-        $newDossier->financementLeaser->montant_solde = $newDossier->financementLeaser->reste;
-        $newDossier->financementLeaser->date_solde = strtotime('2020-03-31');
-
-        $newDossier->save($PDOdb);
-
-        // On garde les même affaires
-        $sql = 'INSERT INTO '.MAIN_DB_PREFIX.'fin_dossier_affaire(fk_fin_dossier, fk_fin_affaire)';
-        $sql.= ' SELECT '.$newDossier->rowid.', fk_fin_affaire';
-        $sql.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier_affaire';
-        $sql.= ' WHERE fk_fin_affaire = '.$d->TLien[0]->fk_fin_affaire;
+        // On load les dossiers
+        $sql = 'SELECT d.rowid';
+        $sql.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier d';
+        $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'fin_dossier_financement df ON (df.fk_fin_dossier = d.rowid)';
+        $sql.= ' WHERE d.entity IN ('.$db->escape($data['entity']).')';
+        $sql.= " AND df.reference LIKE '".$db->escape($data['reference'])."%'"; // On veut les dossiers qui commencent par la référence fournie
 
         $resql = $db->query($sql);
-        if(! $resql) {
-            dol_print_error($db);
-        }
-        $db->free($resql);
+        if(! $resql) dol_print_error($db);
+        else {
+            while($obj = $db->fetch_object($resql)) {
+                $d->load($PDOdb, $obj->rowid, false);
+                if(empty($d->rowid)) {  // Failed to load
+                    $TError[] = $data;
+                    continue;
+                }
+                $d->load_affaire($PDOdb);
 
-        // On déplace les factures fournisseurs vers le nouveau dossier
-        $sql = 'UPDATE '.MAIN_DB_PREFIX.'element_element';
-        $sql.= ' SET fk_source = '.$newDossier->rowid;
-        $sql.= " WHERE sourcetype = 'dossier'";
-        $sql.= ' AND fk_source = '.$d->rowid;
-        $sql.= " AND targettype = 'invoice_supplier'";
+                // On duplique le contrat
+                $newDossier = new TFin_dossier;
+                $newDossier->load($PDOdb, $d->rowid, false);
+                $newDossier->load_affaire($PDOdb);
 
-        $resql = $db->query($sql);
-        if(! $resql) {
-            dol_print_error($db);
-        }
-        $db->free($resql);
+                // On unset les id pour en créer des nouveaux
+                unset($newDossier->id, $newDossier->rowid);
+                unset($newDossier->financement->id, $newDossier->financement->rowid, $newDossier->financement->reference);
+                unset($newDossier->financementLeaser->id, $newDossier->financementLeaser->rowid, $newDossier->financementLeaser->reference);
 
-        // On recalcule l'échéance avec la nouvelle durée
-        /** @var TFin_financement $f */
-        $f = $d->financementLeaser;
-        $beginning = ($f->terme == 1);
-        $dureeRestante = ($f->duree_restante == 0) ? 1 : $f->duree_restante;    // Si le dossier se termine sur l'échéance non payée, il reste donc une période à payer
+                $newDossier->save($PDOdb);  // Saving to get Id
 
-        $e = $d->echeancier($PDOdb, 'LEASER', 1, true, false);
+                // Update references
+                $newDossier->financement->reference = $d->financement->reference.'-COVID';
+                $newDossier->financementLeaser->reference = $d->financementLeaser->reference.'-COVID';
 
-        // Permet de retrouver le bon numéro de période pour recalculer le CRD
-        $periods = null;
-        foreach($e['ligne'] as $iPeriode => $lineData) {
-            if($lineData['date'] == '01/01/2020') {
-                $periods = $iPeriode + 1;
+                // On solde le dossier
+                $newDossier->financement->montant_solde = $newDossier->financement->reste;
+                $newDossier->financement->date_solde = strtotime('2020-03-31');
+                $newDossier->financementLeaser->montant_solde = $newDossier->financementLeaser->reste;
+                $newDossier->financementLeaser->date_solde = strtotime('2020-03-31');
+
+                $newDossier->save($PDOdb);
+
+                // On garde les même affaires
+                $sql = 'INSERT INTO '.MAIN_DB_PREFIX.'fin_dossier_affaire(fk_fin_dossier, fk_fin_affaire)';
+                $sql.= ' SELECT '.$newDossier->rowid.', fk_fin_affaire';
+                $sql.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier_affaire';
+                $sql.= ' WHERE fk_fin_affaire = '.$d->TLien[0]->fk_fin_affaire;
+
+                $resql = $db->query($sql);
+                if(! $resql) {
+                    dol_print_error($db);
+                }
+                $db->free($resql);
+
+                // On déplace les factures fournisseurs vers le nouveau dossier
+                $sql = 'UPDATE '.MAIN_DB_PREFIX.'element_element';
+                $sql.= ' SET fk_source = '.$newDossier->rowid;
+                $sql.= " WHERE sourcetype = 'dossier'";
+                $sql.= ' AND fk_source = '.$d->rowid;
+                $sql.= " AND targettype = 'invoice_supplier'";
+
+                $resql = $db->query($sql);
+                if(! $resql) {
+                    dol_print_error($db);
+                }
+                $db->free($resql);
+
+                // On recalcule l'échéance avec la nouvelle durée
+                /** @var TFin_financement $f */
+                $f = $d->financementLeaser;
+                $beginning = ($f->terme == 1);
+                $dureeRestante = ($f->duree_restante == 0) ? 1 : $f->duree_restante;    // Si le dossier se termine sur l'échéance non payée, il reste donc une période à payer
+
+                $e = $d->echeancier($PDOdb, 'LEASER', 1, true, false);
+
+                // Permet de retrouver le bon numéro de période pour recalculer le CRD
+                $periods = null;
+                foreach($e['ligne'] as $iPeriode => $lineData) {
+                    if($lineData['date'] == '01/01/2020') {
+                        $periods = $iPeriode + 1;
+                    }
+                }
+
+//                $montant = Finance::pv($f->taux / (12 / $f->getiPeriode())/100, $f->duree-$periods, -$f->echeance, $f->reste, $beginning);   // On recalcule le CRD du 31/03/2020
+                $montant = $f->valeur_actuelle($f->duree-$periods); // On recalcule le CRD du 31/03/2020
+                $echeance = Finance::pmt($f->taux / (12 / $f->getiPeriode())/100, $dureeRestante, -$montant, $f->reste, $beginning);    // Calcul de la nouvelle échéance avec le nouveau montant financé
+
+                $d->financementLeaser->duree = $f->duree_restante;
+                $d->financementLeaser->montant = $montant;
+                $d->financementLeaser->echeance = $echeance;
+                $d->financementLeaser->date_debut = strtotime('2020-07-01');
+                $d->financementLeaser->setEcheanceExterne($d->financementLeaser->date_debut);
+                unset($f);
+
+                /** @var TFin_financement $f */
+                $f = $d->financement;
+                $d->financement->reste += $f->echeance;
+
+                $d->save($PDOdb);
+                $nbUpdated++;
             }
         }
 
-//        $montant = Finance::pv($f->taux / (12 / $f->getiPeriode())/100, $f->duree-$periods, -$f->echeance, $f->reste, $beginning);   // On recalcule le CRD du 31/03/2020
-        $montant = $f->valeur_actuelle($f->duree-$periods); // On recalcule le CRD du 31/03/2020
-        $echeance = Finance::pmt($f->taux / (12 / $f->getiPeriode())/100, $dureeRestante, -$montant, $f->reste, $beginning);    // Calcul de la nouvelle échéance avec le nouveau montant financé
 
-        $d->financementLeaser->duree = $f->duree_restante;
-        $d->financementLeaser->montant = $montant;
-        $d->financementLeaser->echeance = $echeance;
-        $d->financementLeaser->date_debut = strtotime('2020-07-01');
-        $d->financementLeaser->setEcheanceExterne($d->financementLeaser->date_debut);
-        unset($f);
-
-        /** @var TFin_financement $f */
-        $f = $d->financement;
-        $d->financement->reste += $f->echeance;
-
-        $d->save($PDOdb);
-        $nbUpdated++;
     }
 
     return $nbUpdated;
