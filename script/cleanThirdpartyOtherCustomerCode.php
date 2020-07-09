2e60b948e47ead84a2c74ddb8b1e3e22e9e4201a
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-07-09 11:14:36 +0200

    FIX : Doublons entre les champs code_client

--- script/cleanThirdpartyOtherCustomerCode.php
+++ script/cleanThirdpartyOtherCustomerCode.php
@@ -1,4 +1,5 @@
 <?php
+$a = microtime(true);
 
 require '../config.php';
 
@@ -6,10 +7,11 @@ $fk_soc = GETPOST('fk_soc', 'int');
 $debug = GETPOST('debug', 'int');
 $commit = GETPOST('commit', 'int');
 
-$sql = 'SELECT fk_object, other_customer_code';
-$sql.= ' FROM '.MAIN_DB_PREFIX.'societe_extrafields';
-$sql.= ' WHERE other_customer_code is not null';
-if(! empty($fk_soc)) $sql.= ' AND fk_object = '.$db->escape($fk_soc);
+$sql = 'SELECT se.fk_object, se.other_customer_code, s.code_client';
+$sql.= ' FROM '.MAIN_DB_PREFIX.'societe_extrafields se';
+$sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'societe s ON (se.fk_object = s.rowid)';
+$sql.= ' WHERE se.other_customer_code is not null';
+if(! empty($fk_soc)) $sql.= ' AND se.fk_object = '.$db->escape($fk_soc);
 
 $resql = $db->query($sql);
 if(! $resql) {
@@ -48,10 +50,19 @@ while($obj = $db->fetch_object($resql)) {
     else {
         $db->begin();
         $TExistingCustomerCode = array_unique(array_filter(explode(';', $obj->other_customer_code)));
-        $strCustomerCode = implode(';', $TExistingCustomerCode);
+
+        // Pour éviter d'avoir des doublons de code entre les 2 champs : On enlève des autres codes clients le code client actuel
+        $TKey = array_keys($TExistingCustomerCode, $obj->code_client);
+        foreach($TKey as $key) unset($TExistingCustomerCode[$key]);
+
+        if(empty($TExistingCustomerCode)) $sqlSetExtrafield = 'other_customer_code = null';
+        else {
+            $strCustomerCode = implode(';', $TExistingCustomerCode);
+            $sqlSetExtrafield= "other_customer_code = '".$db->escape($strCustomerCode)."'";
+        }
 
         if($strCustomerCode != $obj->other_customer_code) {
-            $sqlUpdate = 'UPDATE '.MAIN_DB_PREFIX."societe_extrafields SET other_customer_code = '".$db->escape($strCustomerCode)."' WHERE fk_object = ".$obj->fk_object;
+            $sqlUpdate = 'UPDATE '.MAIN_DB_PREFIX."societe_extrafields SET ".$sqlSetExtrafield." WHERE fk_object = ".$obj->fk_object;
 
             $resUpdate = $db->query($sqlUpdate);
             if(! $resUpdate || empty($commit)) $db->rollback();
@@ -61,9 +72,14 @@ while($obj = $db->fetch_object($resql)) {
             }
         }
     }
+    $db->free($resUpdate);
 }
+$db->free($resql);
+
+$b = microtime(true);
 
 ?>
 <span>Total : <?php echo $nbRow; ?></span><br/>
 <span>Nb update : <?php echo $NbUpdated; ?></span><br/>
-<span>Nb set to null : <?php echo $nbSetToNull; ?></span>
+<span>Nb set to null : <?php echo $nbSetToNull; ?></span><br/>
+<span>Execution time : <?php echo ($b-$a); ?> sec</span>
