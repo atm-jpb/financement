c2a55ac788542e79d17d04e202c8aa453a3a8c90
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-08-04 17:02:55 +0200

    FIX : Reformat cript entity affaire client

--- script/fix-entity-affaire-client.php
+++ script/fix-entity-affaire-client.php
@@ -1,9 +1,10 @@
 <?php
+set_time_limit(0);					// No timeout for this script
 
 require '../config.php';
 dol_include_once('/financement/lib/financement.lib.php');
 
-@set_time_limit(0);					// No timeout for this script
+global $db, $user;
 
 $fk_affaire = GETPOST('fk_affaire', 'int');
 $limit = GETPOST('limit', 'int');
@@ -13,63 +14,73 @@ $sql = 'SELECT a.rowid, a.entity, a.reference, s.rowid as socid, s.entity as ent
 $sql.= ' FROM '.MAIN_DB_PREFIX.'fin_affaire a';
 $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'societe s ON s.rowid = a.fk_soc';
 $sql.= " WHERE a.reference LIKE 'EXT%'";
-$sql.= ' AND a.entity != s.entity';
+$sql.= ' AND a.entity <> s.entity';
+$sql.= ' AND (a.entity not in (1, 2, 3) or s.entity not in (1, 2, 3))';
+$sql.= ' AND (a.entity not in (5, 16) or s.entity not in (5, 16))';
+$sql.= ' AND (a.entity not in (20, 23) or s.entity not in (20, 23))';
 if(! empty($fk_affaire)) $sql.= ' AND a.rowid = '.$db->escape($fk_affaire);
 if(! empty($limit)) $sql.= ' LIMIT '.$db->escape($limit);
 
 $resql = $db->query($sql);
+if(! $resql) {
+    dol_print_error($db);
+    exit;
+}
 
-if($resql) {
-	$i = $j = 0;
-	while ($obj = $db->fetch_object($resql)) {
-		if(in_array($obj->entity, array(1,2,3)) && in_array($obj->entity_soc, array(1,2,3))) continue;
-		if(in_array($obj->entity, array(5,16)) && in_array($obj->entity_soc, array(5,16))) continue;
-		if(in_array($obj->entity, array(20,23)) && in_array($obj->entity_soc, array(20,23))) continue;
-		$i++;
+$i = $j = 0;
+while($obj = $db->fetch_object($resql)) {
+    if(in_array($obj->entity, array(1, 2, 3)) && in_array($obj->entity_soc, array(1, 2, 3))) continue;  // FIXME: Useless with new constraints in SQL
+    if(in_array($obj->entity, array(5, 16)) && in_array($obj->entity_soc, array(5, 16))) continue;      // FIXME: Useless with new constraints in SQL
+    if(in_array($obj->entity, array(20, 23)) && in_array($obj->entity_soc, array(20, 23))) continue;    // FIXME: Useless with new constraints in SQL
+    $i++;
 
-		$TEntityGroup = getOneEntityGroup($obj->entity, 'thirdparty', array(4, 17));
+    $TEntityGroup = getOneEntityGroup($obj->entity, 'thirdparty', array(4, 17));
+    $TKey = array_keys($TEntityGroup, 17);   // On enlève l'entité 17 du tableau car aussi surprenant que cela puisse paraître, des clients existent dans cette entité !!
+    foreach($TKey as $key) unset($TEntityGroup[$key]);
 
-		echo '<hr>AFFAIRE '.$obj->reference.' ('.$obj->rowid.') : ';
-		if(strlen($obj->siren) != 9) {
-			echo 'SIREN INCORRECT';
-			continue;
-		}
+    echo '<hr>AFFAIRE '.$obj->reference.' ('.$obj->rowid.') : ';
+    if(strlen($obj->siren) != 9) {
+        echo 'SIREN INCORRECT';
+        continue;
+    }
 
-		$sql2 = "SELECT s.rowid FROM ".MAIN_DB_PREFIX."societe s WHERE s.entity IN (".implode(',', $TEntityGroup).") AND s.siren = '".$obj->siren."'";
-		$resql2 = $db->query($sql2);
-		if($db->num_rows($resql2) == 0) {
-			echo 'SIREN NON TROUVÉ ('.$sql2.') => ';
-			$soc = new Societe($db);
-			$soc->name = $obj->nom;
-			$soc->address = $obj->address;
-			$soc->zip = $obj->zip;
-			$soc->town = $obj->town;
-			$soc->country_id = $obj->fk_pays;
-			$soc->idprof1 = $obj->siren;
-			$soc->idprof2 = $obj->siret;
-			$soc->entity = $obj->entity;
-			$soc->client = 1;
-			$soc->status = 1;
-			$socid = $soc->create($user);
-			if($socid < 0) {
-				echo 'ERREUR CREATION '.$socid;
-				continue;
-			}
-		} else {
-			$obj2 = $db->fetch_object($resql2);
-			$socid = $obj2->rowid;
-		}
+    $sql2 = 'SELECT s.rowid FROM '.MAIN_DB_PREFIX.'societe s WHERE s.entity IN ('.implode(',', $TEntityGroup).") AND s.siren = '".$obj->siren."'";
+    $resql2 = $db->query($sql2);
+    if($db->num_rows($resql2) == 0) {
+        $db->begin();
 
-		$sql3 = "UPDATE ".MAIN_DB_PREFIX."fin_affaire a SET a.fk_soc = ".$socid." WHERE a.rowid = ".$obj->rowid;
-		echo $sql3;
-		$resql3 = $db->query($sql3);
-		if(!$resql3) {
-			dol_print_error($db);
-		}
-		$j++;
-	}
-} else {
-	echo $sql;
+        echo 'SIREN NON TROUVÉ ('.$sql2.') => ';
+        $soc = new Societe($db);
+        $soc->name = $obj->nom;
+        $soc->address = $obj->address;
+        $soc->zip = $obj->zip;
+        $soc->town = $obj->town;
+        $soc->country_id = $obj->fk_pays;
+        $soc->idprof1 = $obj->siren;
+        $soc->idprof2 = $obj->siret;
+        $soc->entity = $obj->entity;
+        $soc->client = 1;
+        $soc->status = 1;
+        $socid = $soc->create($user);
+        if($socid < 0) {
+            echo 'ERREUR CREATION '.$socid;
+            continue;
+        }
+
+        $db->commit();
+    }
+    else {
+        $obj2 = $db->fetch_object($resql2);
+        $socid = $obj2->rowid;
+    }
+
+    $sql3 = 'UPDATE '.MAIN_DB_PREFIX.'fin_affaire a SET a.fk_soc = '.$socid.' WHERE a.rowid = '.$obj->rowid;
+    echo $sql3;
+    $resql3 = $db->query($sql3);
+    if(! $resql3) {
+        dol_print_error($db);
+    }
+    $j++;
 }
 
 echo '<hr>'.$j.' / '.$i;
