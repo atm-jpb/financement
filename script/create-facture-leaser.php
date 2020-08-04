29b47185a02a3118e561a9059cce2fedae81c270
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-08-04 09:11:29 +0200

    FIX : Create supplier invoice script

--- script/create-facture-leaser.php
+++ script/create-facture-leaser.php
@@ -2,61 +2,56 @@
 	
 define('INC_FROM_CRON_SCRIPT', true);
 
-require('../config.php');
-
+require '../config.php';
+require_once DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.facture.class.php';
 dol_include_once('/financement/class/affaire.class.php');
 dol_include_once('/financement/class/dossier.class.php');
 dol_include_once('/financement/class/grille.class.php');
 
-include_once(DOL_DOCUMENT_ROOT."/fourn/class/fournisseur.facture.class.php");
+global $langs, $user;
 
-$langs->load("main");				// To load language file for default language
+$langs->load('main');				// To load language file for default language
 @set_time_limit(0);					// No timeout for this script
 
 // Load user and its permissions
 if(empty($user->id)) {
-	$result=$user->fetch('',DOL_ADMIN_USER);	// Load user for login 'admin'. Comment line to run as anonymous user.
-	if (! $result > 0) { dol_print_error('',$user->error); exit; }
+	$result = $user->fetch('',DOL_ADMIN_USER);	// Load user for login 'admin'. Comment line to run as anonymous user.
+	if (! $result > 0) { dol_print_error('', $user->error); exit; }
 	$user->getrights();
 }
 
-
-$ATMdb=new TPDOdb;
+$PDOdb = new TPDOdb;
 $fk_dossier = GETPOST('fk_dossier', 'int');
 
 /*
  * Création des factures bon pour facturation
  */
-/*$TabOui = TRequeteCore::get_id_from_what_you_want($ATMdb,MAIN_DB_PREFIX.'fin_dossier_financement',array('okPourFacturation'=>'OUI', 'date_solde'=>'0000-00-00 00:00:00'));
-$TabAuto = TRequeteCore::get_id_from_what_you_want($ATMdb,MAIN_DB_PREFIX.'fin_dossier_financement',array('okPourFacturation'=>'AUTO', 'date_solde'=>'0000-00-00 00:00:00'));
-$TabManuel = TRequeteCore::get_id_from_what_you_want($ATMdb,MAIN_DB_PREFIX.'fin_dossier_financement',array('okPourFacturation'=>'MANUEL', 'date_solde'=>'0000-00-00 00:00:00'));
-$Tab = array_merge($TabOui, $TabAuto, $TabManuel);*/
 
-$sql = 'SELECT rowid FROM '.MAIN_DB_PREFIX.'fin_dossier_financement';
-$sql.= " WHERE (date_solde < '1970-00-00 00:00:00' OR date_solde IS NULL)";
+$sql = 'SELECT rowid';
+$sql.= ' FROM '.MAIN_DB_PREFIX.'fin_dossier_financement';
+$sql.= " WHERE (date_solde < '1970-01-01 00:00:00' OR date_solde IS NULL)";
 $sql.= " AND okPourFacturation IN ('OUI', 'AUTO', 'MANUEL')";
-if(! is_null($fk_dossier)) $sql.= ' AND fk_fin_dossier = '.$fk_dossier;
-$Tab = TRequeteCore::_get_id_by_sql($ATMdb, $sql);
+$sql.= " AND reference IS NOT NULL AND reference <> ''";    // Si le numéro de contrat leaser n'est pas rempli, on passe au dossier suivant
+$sql.= ' AND echeance <> 0.00';
+if(! empty($fk_dossier)) $sql.= ' AND fk_fin_dossier = '.$fk_dossier;
+$Tab = TRequeteCore::_get_id_by_sql($PDOdb, $sql);
 
 foreach($Tab as $id) {
-	$f=new TFin_financement;
-	$f->load($ATMdb, $id);
+	$f = new TFin_financement;
+	$f->load($PDOdb, $id);
 	
-	$d=new TFin_dossier;
-	$d->load($ATMdb, $f->fk_fin_dossier);
+	$d = new TFin_dossier;
+	$d->load($PDOdb, $f->fk_fin_dossier);
 	
-	echo 'Contrat client : '.$d->reference_contrat_interne.' - Contrat leaser : '.$f->reference.' ('.date('d/m/Y',$f->date_prochaine_echeance).' - '.$f->numero_prochaine_echeance.')<br />';
+	echo 'Contrat client : '.$d->reference_contrat_interne.' - Contrat leaser : '.$f->reference.' ('.date('d/m/Y', $f->date_prochaine_echeance).' - '.$f->numero_prochaine_echeance.')<br />';
 	
-	$paid = $f->okPourFacturation == 'MANUEL' ? true : false;
-	// Si le numéro de contrat leaser n'est pas rempli, on passe au dossier suivant
-	if(empty($f->reference)) continue;
-	if(empty($f->echeance)) continue;
+	$paid = $f->okPourFacturation == 'MANUEL';
 	
-	if($d->nature_financement == 'INTERNE') echo $d->generate_factures_leaser($paid);
+	if($d->nature_financement == 'INTERNE') $d->generate_factures_leaser($paid);
 	
-	if($f->okPourFacturation == 'OUI') $f->okPourFacturation='NON';
-	$f->save($ATMdb);
-	$d->save($ATMdb); // Sauvegarde le dossier et le financement leaser qui a été modifié par la génération de facture
+	if($f->okPourFacturation == 'OUI') $f->okPourFacturation = 'NON';
+	$f->save($PDOdb);
+	$d->save($PDOdb); // Sauvegarde le dossier et le financement leaser qui a été modifié par la génération de facture
 
 	echo '<hr>';
 	
