a0c153ef6c4d7c2c4802b039d09f78644ae8e4fd
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-09-30 11:19:09 +0200

    NEW : Export base soldes, ajout d'une colonne

--- export.php
+++ export.php
@@ -9,9 +9,11 @@ dol_include_once('/financement/class/dossier.class.php');
 
 $limit = GETPOST('limit', 'int');
 
+global $langs, $conf, $db;
+
 $langs->load('financement@financement');
 
-$sql = 'SELECT s.reference as ref_simul, cli.nom as client_name, e.label as partenaire, dfcli.reference as num_contrat_client, dflea.reference as num_contrat_leaser, slea.nom as leaser_name';
+$sql = 'SELECT s.reference as ref_simul, cli.nom as client_name, e.label as partenaire, dfcli.reference as num_contrat_client,lea.nom as leaserNameSolde, dflea.reference as num_contrat_leaser, slea.nom as leaser_name';
 $sql .= ', dr.date_debut_periode_client_m1, dr.date_fin_periode_client_m1, dr.solde_vendeur_m1, dr.solde_banque_m1, dr.solde_banque_nr_m1';   // Prev
 $sql .= ', dr.date_debut_periode_client, dr.date_fin_periode_client, dr.solde_vendeur, dr.solde_banque, dr.solde_banque_nr';   // Curr
 $sql .= ', dr.date_debut_periode_client_p1, dr.date_fin_periode_client_p1, dr.solde_vendeur_p1, dr.solde_banque_p1, dr.solde_banque_nr_p1';   // Next
@@ -24,6 +26,7 @@ $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'fin_dossier_affaire da ON (dr.fk_dossier =
 $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'fin_affaire a ON (da.fk_fin_affaire = a.rowid)';
 $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'societe cli ON (a.fk_soc = cli.rowid)';
 $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX."fin_dossier_financement dflea ON (dr.fk_dossier = dflea.fk_fin_dossier AND dflea.type = 'LEASER')";
+$sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'llx_societe lea ON (dflea.fk_soc = lea.rowid)';
 $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'societe slea ON (s.fk_leaser = slea.rowid)';
 $sql.= ' WHERE s.entity IN ('.getEntity('fin_simulation', true).')';
 $sql.= " AND s.accord = 'OK'";
@@ -38,7 +41,7 @@ if(! $resql) {
     exit;
 }
 
-$THead = array(
+$THead = [
     'Num_simul',
     'client',
     'partenaire',
@@ -51,7 +54,7 @@ $THead = array(
     'date_fin_periode',
     'type_solde',
     'leaser_accord'
-);
+];
 
 if($conf->entity > 1) $path = DOL_DATA_ROOT.'/'.$conf->entity.'/financement/export/soldes';
 else $path = DOL_DATA_ROOT.'/financement/export/soldes';
@@ -64,13 +67,14 @@ fputcsv($f, $THead, ';');
 
 while($obj = $db->fetch_object($resql)) {
     // ça c'est la partie facile
-    $TData = array(
+    $TData = [
         $obj->ref_simul,
         $obj->client_name,
         $obj->partenaire,
         $obj->num_contrat_client,
+        $obj->leaserNameSolde,
         $obj->num_contrat_leaser,
-    );
+    ];
 
     $solde = TSimulation::getTypeSolde($obj->rowid, $obj->fk_dossier, $obj->fk_leaser);
 
